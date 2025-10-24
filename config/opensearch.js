57133af1e03a2ceaeef41dc7e2a576468e864458
Adrian Guereque <aguereque96@gmail.com>
2025-10-24 16:15:52 +1100

    Fix: opensearch connection 2

--- config/opensearch.js
+++ config/opensearch.js
@@ -1,51 +1,47 @@
 const { Client } = require('@opensearch-project/opensearch');
-const { defaultProvider } = require('@aws-sdk/credential-provider-node');
-const { SignatureV4 } = require('@aws-sdk/signature-v4');
-const { defaultRegionInfoProvider } = require('@aws-sdk/config-resolver');
 const { NodeHttpHandler } = require('@aws-sdk/node-http-handler');
 const { HttpRequest } = require('@aws-sdk/protocol-http');
+const { defaultProvider } = require('@aws-sdk/credential-provider-node');
+const { SignatureV4 } = require('@aws-sdk/signature-v4');
 const { Sha256 } = require('@aws-crypto/sha256-js');
-const { fromEnv } = require('@aws-sdk/credential-provider-env');
 
-// Initialize AWS Signature V4 signer
+const region = 'ap-southeast-2';
+const endpoint = 'https://vpc-llmmq-cv3mo2b7mblxoxptu7xwf76tm4.aos.ap-southeast-2.on.aws';
+
 const signer = new SignatureV4({
   credentials: defaultProvider(),
-  region: 'ap-southeast-2',
   service: 'es',
+  region,
   sha256: Sha256,
 });
 
-// Create OpenSearch client
+class AwsSigV4Connection {
+  constructor() {
+    this.httpClient = new NodeHttpHandler();
+  }
+
+  async request(params) {
+    const { method, path, body } = params;
+
+    const request = new HttpRequest({
+      body: body ? JSON.stringify(body) : undefined,
+      headers: { 'Content-Type': 'application/json' },
+      hostname: endpoint.replace(/^https?:\/\//, ''),
+      method,
+      path,
+      protocol: 'https:',
+    });
+
+    const signedRequest = await signer.sign(request);
+    const { response } = await this.httpClient.handle(signedRequest);
+
+    return response;
+  }
+}
+
 const client = new Client({
-  node: 'https://vpc-llmmq-cv3mo2b7mblxoxptu7xwf76tm4.aos.ap-southeast-2.on.aws',
-  Connection: class {
-    constructor() {
-      this.httpClient = new NodeHttpHandler();
-    }
-
-    async request(params) {
-      const { method, body, path } = params;
-      const request = new HttpRequest({
-        body,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        method,
-        hostname: 'vpc-llmmq-cv3mo2b7mblxoxptu7xwf76tm4.aos.ap-southeast-2.on.aws',
-        path,
-        protocol: 'https:',
-      });
-
-      const signedRequest = await signer.sign(request);
-      const { headers } = await this.httpClient.handle(signedRequest);
-
-      return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({ message: 'Request successful' }),
-      };
-    }
-  },
+  node: endpoint,
+  Connection: AwsSigV4Connection,
 });
 
 module.exports = client;
