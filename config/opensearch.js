2bf6cb4d085de5aead90fb9e2fb0958df47b1bd6
Adrian Guereque <aguereque96@gmail.com>
2025-10-24 16:06:37 +1100

    Fix: Opensearch connection

--- config/opensearch.js
+++ config/opensearch.js
@@ -1,16 +1,51 @@
-//config/opensearch.js
 const { Client } = require('@opensearch-project/opensearch');
-const AWS = require('aws-sdk');
-const createAwsConnector = require('@opensearch-project/opensearch/aws');
+const { defaultProvider } = require('@aws-sdk/credential-provider-node');
+const { SignatureV4 } = require('@aws-sdk/signature-v4');
+const { defaultRegionInfoProvider } = require('@aws-sdk/config-resolver');
+const { NodeHttpHandler } = require('@aws-sdk/node-http-handler');
+const { HttpRequest } = require('@aws-sdk/protocol-http');
+const { Sha256 } = require('@aws-crypto/sha256-js');
+const { fromEnv } = require('@aws-sdk/credential-provider-env');
 
-const credentials = new AWS.EnvironmentCredentials('AWS'); // Uses EC2 role creds
+// Initialize AWS Signature V4 signer
+const signer = new SignatureV4({
+  credentials: defaultProvider(),
+  region: 'ap-southeast-2',
+  service: 'es',
+  sha256: Sha256,
+});
 
+// Create OpenSearch client
 const client = new Client({
-  ...createAwsConnector({
-    region: 'ap-southeast-2',
-    credentials,
-  }),
   node: 'https://vpc-llmmq-cv3mo2b7mblxoxptu7xwf76tm4.aos.ap-southeast-2.on.aws',
+  Connection: class {
+    constructor() {
+      this.httpClient = new NodeHttpHandler();
+    }
+
+    async request(params) {
+      const { method, body, path } = params;
+      const request = new HttpRequest({
+        body,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method,
+        hostname: 'vpc-llmmq-cv3mo2b7mblxoxptu7xwf76tm4.aos.ap-southeast-2.on.aws',
+        path,
+        protocol: 'https:',
+      });
+
+      const signedRequest = await signer.sign(request);
+      const { headers } = await this.httpClient.handle(signedRequest);
+
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({ message: 'Request successful' }),
+      };
+    }
+  },
 });
 
 module.exports = client;
